58b32027b9b3ea2c0a9aae82c7036dd1c4dbd4f8
Vladyslav <vladislavshvetsua@gmail.com>
2023-05-25 17:35:27 +0200

    Header and footer includes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,15 @@
 <?php
+    //Load config
+
+    require_once 'config/config.php';
+
+
     //Load libaries
-    require_once('libraries/Core.php');
-    require_once('libraries/Controller.php');
-    require_once('libraries/Database.php');
+    //require_once('libraries/Core.php');
+    //require_once('libraries/Controller.php');
+    //require_once('libraries/Database.php');
+
+    //Autoload Core Libraries
+    spl_autoload_register(function($classname){
+        require_once('libraries/'.$classname.'.php');
+    });
---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+//App root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URL Root
+    define('URLROOT', 'http://localhost/traversmvc');
+    
+    //Site name
+    define('SITENAME', 'TraversyMVC');
